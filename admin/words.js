464771c9fccad1144bfd7bc237e8b410748fd571
Simon B <notmyham@googlemail.com>
2020-04-14 23:13:19 +0200

    Initial Commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'solarmax-iobroker-adapter adapter settings': {
+		'en': 'Adapter settings for solarmax-iobroker-adapter',
+		'de': 'Adaptereinstellungen für solarmax-iobroker-adapter',
+		'ru': 'Настройки адаптера для solarmax-iobroker-adapter',
+		'pt': 'Configurações do adaptador para solarmax-iobroker-adapter',
+		'nl': 'Adapterinstellingen voor solarmax-iobroker-adapter',
+		'fr': "Paramètres d'adaptateur pour solarmax-iobroker-adapter",
+		'it': "Impostazioni dell'adattatore per solarmax-iobroker-adapter",
+		'es': 'Ajustes del adaptador para solarmax-iobroker-adapter',
+		'pl': 'Ustawienia adaptera dla solarmax-iobroker-adapter',
+		'zh-cn': 'solarmax-iobroker-adapter的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
